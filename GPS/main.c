727c87fd5ef667dfb9a454e633692430bea7216b
20161104607 <a20161104607@admindeiMac-35.local>
2017-06-19 09:52:16 +0800

    gps
    开始

--- GPS/main.c
+++ GPS/main.c
@@ -7,9 +7,25 @@
 //
 
 #include <stdio.h>
+    
+    int main()
+    {
+        int  ;
+        char ch[62];
+        FILE *fp1,*fp2;
+        fp1=fopen("//Users//a20161104607//Desktop//GPS//GPS170510.log ","w");
+        fp2=fopen("//Users//a20161104607//Desktop//GPS//GPS170510.log ","r");
+        if(fp2==NULL)
+        {
+            printf("文件输出错误");
+        }
+        else
+        {
+            fscanf(fp2,"$GPRMC,,%6s,%c,%1f,%c,%1f,%c,%1f,%1f,%6s,%s",time,7gps,&latitude1,&longitude1,&v,&cours,year,s);
+            if(gps==‘A’)
+            {
+        
+            }
+        }
+    }
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
